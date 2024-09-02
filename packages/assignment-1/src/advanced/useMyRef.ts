4c8744a9908aa02beab62e6f3b23abb08d3c778d
HyunWoo Choi <116826162+chhw130@users.noreply.github.com>
2024-09-02 21:19:09 +0900

    feat : useMyRef

--- packages/assignment-1/src/advanced/useMyRef.ts
+++ packages/assignment-1/src/advanced/useMyRef.ts
@@ -1,3 +1,9 @@
+import { useMemo } from "react";
+
 export function useMyRef<T>(initValue: T | null) {
-  return { current: initValue }
+  const value = useMemo(() => {
+    return { current: initValue };
+  }, [initValue]);
+
+  return value;
 }
