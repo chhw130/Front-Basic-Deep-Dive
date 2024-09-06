2ad3e95ca83119b04b544b4243502438b95f63e5
chhw130 <527coco@naver.com>
2024-09-06 21:11:22 +0900

    feat : memo function

--- packages/assignment-2/src/advanced/advanced.jsx
+++ packages/assignment-2/src/advanced/advanced.jsx
@@ -1,13 +1,37 @@
 import { createContext, useContext, useState } from "react";
 
-export const memo1 = (fn) => fn();
+export const memo1 = (() => {
+  const cache = new Map();
 
-export const memo2 = (fn) => fn();
+  return (fn) => {
+    if (cache.get(JSON.stringify(fn))) {
+      return cache.get(JSON.stringify(fn));
+    }
 
+    cache.set(JSON.stringify(fn), fn());
+
+    return cache.get(JSON.stringify(fn));
+  };
+})();
+
+export const memo2 = (() => {
+  const cache = new Map();
+
+  return (fn, keys) => {
+    const [key] = keys;
+    if (cache.get(key)) {
+      return cache.get(key);
+    }
+
+    cache.set(key, fn());
+
+    return cache.get(key);
+  };
+})();
 
 export const useCustomState = (initValue) => {
   return useState(initValue);
-}
+};
 
 const textContextDefaultValue = {
   user: null,
@@ -27,36 +51,27 @@ export const TestContextProvider = ({ children }) => {
     <TestContext.Provider value={{ value, setValue }}>
       {children}
     </TestContext.Provider>
-  )
-}
+  );
+};
 
 const useTestContext = () => {
   return useContext(TestContext);
-}
+};
 
 export const useUser = () => {
   const { value, setValue } = useTestContext();
 
-  return [
-    value.user,
-    (user) => setValue({ ...value, user })
-  ];
-}
+  return [value.user, (user) => setValue({ ...value, user })];
+};
 
 export const useCounter = () => {
   const { value, setValue } = useTestContext();
 
-  return [
-    value.count,
-    (count) => setValue({ ...value, count })
-  ];
-}
+  return [value.count, (count) => setValue({ ...value, count })];
+};
 
 export const useTodoItems = () => {
   const { value, setValue } = useTestContext();
 
-  return [
-    value.todoItems,
-    (todoItems) => setValue({ ...value, todoItems })
-  ];
-}
+  return [value.todoItems, (todoItems) => setValue({ ...value, todoItems })];
+};
