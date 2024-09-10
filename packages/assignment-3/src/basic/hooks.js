73de167a3b58c517b6aee465d9844a545736715d
chhw130 <527coco@naver.com>
2024-09-10 22:02:08 +0900

    feat : useState with closure

--- packages/assignment-3/src/basic/hooks.js
+++ packages/assignment-3/src/basic/hooks.js
@@ -1,6 +1,34 @@
 export function createHooks(callback) {
+  // const globalState = [];
+  // let globalStateIdx = 0;
+
+  const stateContext = {
+    idx: 0,
+    state: [],
+  };
+
   const useState = (initState) => {
-    return [];
+    // 게으른 초기화도 반영
+    const state = typeof initState === "function" ? initState() : initState;
+
+    const tempIdx = stateContext.idx;
+
+    // 기존 값이 있다면 그대로
+    stateContext.state[tempIdx] = stateContext.state[tempIdx] || state;
+
+    const setState = (newState) => {
+      const currentIdx = tempIdx;
+
+      if (!Object.is(stateContext.state[currentIdx], newState)) {
+        stateContext.state[currentIdx] = newState;
+        callback();
+        return;
+      }
+    };
+
+    stateContext.idx++;
+
+    return [stateContext.state[tempIdx], setState];
   };
 
   const useMemo = (fn, refs) => {
@@ -8,8 +36,9 @@ export function createHooks(callback) {
   };
 
   const resetContext = () => {
-
-  }
+    // 문맥을 초기화 하여 순서를 보장함
+    stateContext.idx = 0;
+  };
 
   return { useState, useMemo, resetContext };
 }
